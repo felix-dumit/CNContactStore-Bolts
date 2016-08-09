9bf2e72dccb04447fb88e1a0002ae0d23e4d3e00
Felix Dumit <felix.dumit@gmail.com>
2016-08-09 11:41:59 -0700

    whitespace changes

--- CNContactStore+Bolts.h
+++ CNContactStore+Bolts.h
@@ -13,11 +13,11 @@
 
 - (nonnull BFTask<NSNumber *> *)requestAccessForEntityType:(CNEntityType)entityType;
 
-- (nonnull BFTask< NSArray<CNContact *> *  > *)unifiedContactsMatchingPredicate:(nonnull NSPredicate *)predicate keysToFetch:(nullable NSArray<id<CNKeyDescriptor> > *)keys;
+- (nonnull BFTask<NSArray<CNContact *> *> *)unifiedContactsMatchingPredicate:(nonnull NSPredicate *)predicate keysToFetch:(nullable NSArray<id<CNKeyDescriptor> > *)keys;
 
 - (nonnull BFTask<CNContact *> *)unifiedContactWithIdentifier:(nonnull NSString *)identifier keysToFetch:(nullable NSArray<id<CNKeyDescriptor> > *)keys;
 
-- (nonnull BFTask<CNContact *>*)unifiedMeContactWithKeysToFetch:(nullable NSArray<id<CNKeyDescriptor>> *)keys NS_AVAILABLE(10_11, NA) __WATCHOS_PROHIBITED;
+- (nonnull BFTask<CNContact *> *)unifiedMeContactWithKeysToFetch:(nullable NSArray<id<CNKeyDescriptor>> *)keys NS_AVAILABLE(10_11, NA) __WATCHOS_PROHIBITED;
 
 
 - (nonnull BFTask<NSNumber *> *)enumerateContactsWithFetchRequest:(nonnull CNContactFetchRequest *)fetchRequest usingBlock:(nullable void (^)(CNContact *_Nonnull, BOOL *_Nonnull))block;
@@ -27,7 +27,5 @@
 - (nonnull BFTask<NSArray<CNContainer *> *> *)containersMatchingPredicate:(nonnull NSPredicate *)predicate;
 
 - (nonnull BFTask<NSNumber *> *)executeSaveRequest:(nonnull CNSaveRequest *)saveRequest;
-
-
 
 @end
